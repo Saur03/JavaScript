b9c83e730683248913678ca9472a39552a340011
Saur03 <chawlasaurabh97@gmail.com>
2024-02-18 20:41:53 -0500

    Assignment 4 of JS with defer use only

---
+++ Assignmentd4 of JavaScript/Assignment4.js
@@ -0,0 +1,11 @@
+/*
+Assignment 4
+Name- Saurabh Chawla
+StudentID- 0838092
+Date- 16 February 2024
+*/
+
+//create a variable that contains a string value with your name in it.
+let nameOfMine = "Saurabh Chawla";
+console.log(nameOfMine);
+document.querySelector('h1').textContent = nameOfMine;
