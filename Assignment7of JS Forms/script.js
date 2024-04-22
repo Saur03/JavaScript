366058b5ad77f282840e0de9384971a7dffc12fb
Saur03 <chawlasaurabh97@gmail.com>
2024-04-21 21:17:17 -0400

    some changes done in the code

--- Assignment7of JS Forms/script.js
+++ Assignment7of JS Forms/script.js
@@ -4,7 +4,7 @@
 	Assignment 7
 	{your name, student number, and date here}
 	Name- Saurabh Chawla
-	Student number- 088092
+	Student number- 0838092
 	Date- 12 April 2024
 	Assignment name- Forms assignment and assignment 7
 */
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function(){
 	let form = document.querySelector('form');
 	// the password input field in the HTML has a name attribute of "password".
 	// Since, I am selecting it by its name attribute, that is the reason, I have used the below statement to target it.
-	let password = document.querySelector('input[name="password"]');
+	let pass = document.querySelector('input[name="password"]');
 	// the below 2 lines, I am targeting it by their IDs.
 	let city = document.getElementById('city');
 	let comment = document.getElementById('comment');
@@ -25,13 +25,9 @@ document.addEventListener("DOMContentLoaded", function(){
 	let checkbox = document.querySelector('input[name="accept"]');
 	let send = document.querySelector("button[name='send']");
 
-	// for food part
-	let food = document.querySelector("input[name='food']:checked");
-
-
 	// Step 2. When the page loads do the following:
 	// add the password value "monkey"
-	password.value = 'monkey';
+	pass.value = 'monkey';
 	// select the favourite city "New York"
 	city.value = 'New York';
 	
@@ -55,46 +51,57 @@ document.addEventListener("DOMContentLoaded", function(){
 	// shown change the button to say "Hide" and visa versa. (Hint: change the field type to "text") Also,
 	// prevent the form from submitting. 
 	show.addEventListener('click', function (){
-		if (password.type === 'password'){
-			password.type = 'text';
+		if (pass.type === 'password'){
+			pass.type = 'text';
 			show.textContent = 'hide';
 		}
 		else {
-			password.type = 'password';
+			pass.type = 'password';
 			show.textContent = 'Show';
 		}
 	});
 
-	form.addEventListener('submit', function (event){
-		event.preventDefault(); // prevent the form from submitting
+	// Also prevent the form from submitting
 
-		// Step 6. When the form submits: 
-		// check that the favourite food is "Apple". If not, do not submit the form and show an alert
-		// box that tells the user what food they should like. (Hint: remember that the value of a form field is always a string.)
-		// See if any text is selected in the textarea. If it is, do not submit the form and show an alert
-		// box asking the user why they selected that text.
-		if (!food || food.value !== "2"){
-			alert("What food you should like?");
-			return;
-		}
-		// we are checking if any text is selected or not and using the below lines from the class exercise examples.
+	form.addEventListener('submit', function (event){
+		event.preventDefault();
+	
+	});
+
+
+
+	// Step 5. When the checkbox is checked enable the "Send Data" button, when unchecked disable it.
+	// I have done the below step using the class exercise which our professor have told on 16 April 2024 in the class.
+	checkbox.addEventListener('change', function(){
+		send.disabled = !checkbox.checked;
+	});
+
+	form.addEventListener('submit', function (event){
+        // Step 6. When the form submits: 
+        // check that the favourite food is "Apple". If not, do not submit the form and show an alert
+        // box that tells the user what food they should like. (Hint: remember that the value of a form field is always a string.)
+        // See if any text is selected in the textarea. If it is, do not submit the form and show an alert
+        // box asking the user why they selected that text.
+
+        // Check favorite food
+        let food = document.querySelector("input[name='food']:checked");
+        if (!food || food.value !== "2"){
+            event.preventDefault();
+            alert("What food you should like?");
+            return;
+        }
+
+        // checking if the any text is selected in the textarea and 
+		// done the below part using the class exercise which our professor have told us. 
 		let selectedText = window.getSelection().toString().trim();
-		
 		if(selectedText != ''){
 			alert("Why did you selected that text?");
         	return;
     	}
+        
+		console.log('Form submitted successfully.');
 
-		
-		console.log('Form submitted successfully.')
-
-	});
-
-	// Step 5. When the checkbox is checked enable the "Send Data" button, when unchecked disable it.
-	// I have done the below step using the class exercise which our professor have done today(16 April 2024) in
-	checkbox.addEventListener('change', function(){
-		send.disabled = !checkbox.checked;
-	});
+    });
 
 	// Step 7. When the user enters their first name (change event) save the value to localStorage. When the
 	// page loads, check to see if there is a value stored. If so, fill in the first name with that value. (This
@@ -105,12 +112,13 @@ document.addEventListener("DOMContentLoaded", function(){
 	// Save the first name to localStorage using change event
 	name.addEventListener('change', function(){
 		localStorage.setItem("firstName", name.value);
-	})
+	});
 
 	// checking here if there is a value stored.
 	let valueStore = localStorage.getItem("firstName");
 	if (valueStore) {
 		name.value = valueStore;
 	}
+
 });
 
